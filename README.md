ea18851847dfa7b25972bdbfe0eecef13d138d5c
Roller Davic <rollerdavic@gmail.com>
2019-02-01 14:22:37 +0700

    Update

--- README.md
+++ README.md
@@ -1,27 +1,17 @@
-# Gekko [![npm](https://img.shields.io/npm/dm/gekko.svg)]() [![Build Status](https://travis-ci.org/askmike/gekko.png)](https://travis-ci.org/askmike/gekko) [![Build status](https://ci.appveyor.com/api/projects/status/github/askmike/gekko?branch=stable&svg=true)](https://ci.appveyor.com/project/askmike/gekko)
+#Install
+	#Ubuntu System
+		apt install curl git screen
+		curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+		apt-get install -y nodejs
+		git clone https://github.com/vtnplus/gekko0.6.git gekko
+		cd gekko
+		npm install
+		cd ./exchange
+		npm install
+		cd ../
+		npm start
+	#Default install run with UI
+	You can run muti with screen
 
-![Gordon Gekko](http://mikevanrossum.nl/static/gekko.jpg)
-
-*The most valuable commodity I know of is information.*
-
--Gordon Gekko
-
-Gekko is a Bitcoin TA trading and backtesting platform that connects to popular Bitcoin exchanges. It is written in JavaScript and runs on [Node.js](http://nodejs.org).
-
-*Use Gekko at your own risk.*
-
-## Documentation
-
-See [the documentation website](https://gekko.wizb.it/docs/introduction/about_gekko.html).
-
-## Installation & Usage
-
-See [the installing Gekko doc](https://gekko.wizb.it/docs/installation/installing_gekko.html).
-
-## Community & Support
-
-Gekko has [a forum](https://forum.gekko.wizb.it/) that is the place for discussions on using Gekko, automated trading and exchanges. In case you rather want to chat in realtime about Gekko feel free to join the [Gekko Support Discord](https://discord.gg/26wMygt).
-
-## Final
-
-If Gekko helped you in any way, you can always leave me a tip at (BTC) 13r1jyivitShUiv9FJvjLH7Nh1ZZptumwW
+		screen -S "Coin1"
+		node gekko.js -c "your config.js"
---
+++ config.js
@@ -0,0 +1,251 @@
+// Everything is explained here:
+// @link https://gekko.wizb.it/docs/commandline/plugins.html
+const toml = require('toml');
+const fs = require('fs');
+const _ = require('lodash');
+const secret = require("./SECRET-api-keys.json");
+var config = {};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                          GENERAL SETTINGS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+config.debug = true; // for additional logging / debugging
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                         WATCHING A MARKET
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+config.watch = {
+
+  // see https://gekko.wizb.it/docs/introduction/supported_exchanges.html
+  exchange: 'binance',
+  currency: 'BTC',
+  asset: 'LUN',
+
+  // You can set your own tickrate (refresh rate).
+  // If you don't set it, the defaults are 2 sec for
+  // okcoin and 20 sec for all other exchanges.
+  // tickrate: 20
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING TRADING ADVICE
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+config.tradingAdvisor = {
+  enabled: true,
+  method: 'BNB_Trader',
+  candleSize: 1,
+  historySize: 60,
+}
+
+config.BNB_Trader = toml.parse(fs.readFileSync(__dirname + "/config/strategies/"+config.tradingAdvisor.method+".toml", 'utf8'));
+
+// settings for other strategies can be found at the bottom, note that only
+// one strategy is active per gekko, the other settings are ignored.
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING PLUGINS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// do you want Gekko to simulate the profit of the strategy's own advice?
+config.paperTrader = {
+  enabled: false,
+  // report the profit in the currency or the asset?
+  reportInCurrency: true,
+  // start balance, on what the current balance is compared with
+  simulationBalance: {
+    // these are in the unit types configured in the watcher.
+    asset: 1,
+    currency: 100,
+  },
+  // how much fee in % does each trade cost?
+  feeMaker: 0.15,
+  feeTaker: 0.25,
+  feeUsing: 'maker',
+  // how much slippage/spread should Gekko assume per trade?
+  slippage: 0.05,
+}
+
+config.performanceAnalyzer = {
+  enabled: true,
+  riskFreeReturn: 5
+}
+
+// Want Gekko to perform real trades on buy or sell advice?
+// Enabling this will activate trades for the market being
+// watched by `config.watch`.
+config.trader = {
+  enabled: true,
+  key: secret.binance.key,
+  secret: secret.binance.secret,
+  username: '', // your username, only required for specific exchanges.
+  passphrase: '', // GDAX, requires a passphrase.
+}
+
+config.eventLogger = {
+  enabled: false,
+  // optionally pass a whitelist of events to log, if not past
+  // the eventLogger will log _all_ events.
+  // whitelist: ['portfolioChange', 'portfolioValueChange']
+}
+
+config.pushover = {
+  enabled: false,
+  sendPushoverOnStart: false,
+  muteSoft: true, // disable advice printout if it's soft
+  tag: '[GEKKO]',
+  key: '',
+  user: ''
+}
+
+// want Gekko to send a mail on buy or sell advice?
+config.mailer = {
+  enabled: false, // Send Emails if true, false to turn off
+  sendMailOnStart: true, // Send 'Gekko starting' message if true, not if false
+
+  email: '', // Your Gmail address
+  muteSoft: true, // disable advice printout if it's soft
+
+  // You don't have to set your password here, if you leave it blank we will ask it
+  // when Gekko's starts.
+  //
+  // NOTE: Gekko is an open source project < https://github.com/askmike/gekko >,
+  // make sure you looked at the code or trust the maintainer of this bot when you
+  // fill in your email and password.
+  //
+  // WARNING: If you have NOT downloaded Gekko from the github page above we CANNOT
+  // guarantuee that your email address & password are safe!
+
+  password: '', // Your Gmail Password - if not supplied Gekko will prompt on startup.
+
+  tag: '[GEKKO] ', // Prefix all email subject lines with this
+
+  //       ADVANCED MAIL SETTINGS
+  // you can leave those as is if you
+  // just want to use Gmail
+
+  server: 'smtp.gmail.com', // The name of YOUR outbound (SMTP) mail server.
+  smtpauth: true, // Does SMTP server require authentication (true for Gmail)
+  // The following 3 values default to the Email (above) if left blank
+  user: '', // Your Email server user name - usually your full Email address 'me@mydomain.com'
+  from: '', // 'me@mydomain.com'
+  to: '', // 'me@somedomain.com, me@someotherdomain.com'
+  ssl: true, // Use SSL (true for Gmail)
+  port: '', // Set if you don't want to use the default port
+}
+
+
+config.candleWriter = {
+  enabled: true
+}
+
+config.adviceWriter = {
+  enabled: false,
+  muteSoft: true,
+}
+
+config.backtestResultExporter = {
+  enabled: false,
+  writeToDisk: false,
+  data: {
+    stratUpdates: false,
+    portfolioValues: true,
+    stratCandles: true,
+    roundtrips: true,
+    trades: true
+  }
+}
+
+config.candleUploader = {
+  enabled: false,
+  url: '',
+  apiKey: ''
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING ADAPTER
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+config.adapter = 'sqlite';
+
+config.sqlite = {
+  path: 'plugins/sqlite',
+
+  dataDirectory: 'history',
+  version: 0.1,
+
+  journalMode: require('./web/isWindows.js') ? 'DELETE' : 'WAL',
+
+  dependencies: []
+}
+
+// Postgres adapter example config (please note: requires postgres >= 9.5):
+config.postgresql = {
+  path: 'plugins/postgresql',
+  version: 0.1,
+  connectionString: 'postgres://user:pass@localhost:5432', // if default port
+  database: null, // if set, we'll put all tables into a single database.
+  schema: 'public',
+  dependencies: [{
+    module: 'pg',
+    version: '7.4.3'
+  }]
+}
+
+// Mongodb adapter, requires mongodb >= 3.3 (no version earlier tested)
+config.mongodb = {
+  path: 'plugins/mongodb',
+  version: 0.1,
+  connectionString: 'mongodb://localhost/gekko', // connection to mongodb server
+  dependencies: [{
+    module: 'mongojs',
+    version: '2.4.0'
+  }]
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING BACKTESTING
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Note that these settings are only used in backtesting mode, see here:
+// @link: https://gekko.wizb.it/docs/commandline/backtesting.html
+
+config.backtest = {
+  daterange: 'scan',
+  // daterange: {
+  //   from: "2018-03-01",
+  //   to: "2018-04-28"
+  //},
+  batchSize: 50
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING IMPORTING
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+config.importer = {
+  daterange: {
+    // NOTE: these dates are in UTC
+    from: "2017-11-01 00:00:00",
+    to: "2017-11-20 00:00:00"
+  }
+}
+
+
+
+// set this to true if you understand that Gekko will
+// invest according to how you configured the indicators.
+// None of the advice in the output is Gekko telling you
+// to take a certain position. Instead it is the result
+// of running the indicators you configured automatically.
+//
+// In other words: Gekko automates your trading strategies,
+// it doesn't advice on itself, only set to true if you truly
+// understand this.
+//
+// Not sure? Read this first: https://github.com/askmike/gekko/issues/201
+config['autostart'] = true;
+
+module.exports = config;
