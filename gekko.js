9caef9d9c368fdbb5fc796e3439e0f45e2a575bc
macq@MacQs-MacBook.(none) <macq@MacQs-MacBook.local>
2019-02-07 21:21:32 +0700

    Remove Web API

--- gekko.js
+++ gekko.js
@@ -39,9 +39,11 @@ console.log('\tI\'m gonna make you rich, Bud Fox.', '\n\n');
 
 const dirs = util.dirs();
 
+/*
 if(util.launchUI()) {
   return require(util.dirs().web + 'server');
 }
+*/
 
 const pipeline = require(dirs.core + 'pipeline');
 const config = util.getConfig();
