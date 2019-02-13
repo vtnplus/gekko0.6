250af4cdf723e274fbf8d50f3bd14b6384fc2f9c
Roller Davic <rollerdavic@gmail.com>
2019-02-13 21:30:11 +0700

    Update

--- plugins/trader/trader.js
+++ plugins/trader/trader.js
@@ -106,11 +106,7 @@ Trader.prototype.writeCacheTrader = function(type,price,amount, gtdate){
         
     }else{
         defaultData = '{"asset":0,"currency" : 0, "amount" : 0, "buyPrice" : 0, "buyAmount" : 0, "buyGtdate" : "", "sellPrice" : 0, "sellAmount" : 0, "sellGtdate" : ""}';
-        fsw.writeFile(filecache, defaultData, function (err) {
-            if (err) 
-                return console.log(err);
-                console.log('Create File Config');
-        });
+        console.log('Create File Config');
         readCache = JSON.parse(defaultData);
     }
 
