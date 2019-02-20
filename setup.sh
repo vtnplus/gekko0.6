0d02745fc0388d4cd2fa8028753207b38ddcd5ac
Roller Davic <rollerdavic@gmail.com>
2019-02-20 21:30:29 +0700

    Update Service

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 apt install -y curl git screen
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 apt-get install -y nodejs
+cd /opt
+git clone https://github.com/vtnplus/gekko0.6.git aitrader
+cd ./aitrader
 npm install
 cd ./exchange
 npm install
