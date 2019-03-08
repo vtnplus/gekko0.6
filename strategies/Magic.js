b317bf18431e86a1c2d7343f58548e9d52ab2bf8
Roller Davic <rollerdavic@gmail.com>
2019-03-09 01:01:53 +0700

    Update Magic

---
+++ strategies/Magic.js
@@ -0,0 +1,334 @@
+const log = require('../core/log.js');
+const config = require('../core/util.js').getConfig();
+const _ = require('lodash');
+const request = require('request');
+const fs = require('fs');
+
+// strategy
+var strat = {
+	init : function(){
+		this.trend = {
+			
+			direction: 'none',
+			
+		}
+		this.order = {
+			"start_amount" : 0,
+			"finish_amount" : 0,
+			"buy_price" : 0,
+			"sell_price" : 0,
+			"amount" : 0,
+			"balance" : 0,
+			"date" : 0,
+			"block_time" : 0,
+			"block_stoplost_number" : 1
+		}
+
+		this.addTalibIndicator('maSlow', 'sma', {optInTimePeriod :this.settings.SMA.long });
+		this.addTalibIndicator('maFast', 'sma', {optInTimePeriod :this.settings.SMA.short });
+
+		this.addTalibIndicator('BULL_RSI', 'rsi', {optInTimePeriod : this.settings.BULL.rsi});
+		this.addTalibIndicator('BEAR_RSI', 'rsi', {optInTimePeriod : this.settings.BEAR.rsi});
+		this.addTalibIndicator('bbands', 'bbands', this.settings.BBands);
+		this.addTalibIndicator('adx', 'adx', {optInTimePeriod : this.settings.ADX.optInTimePeriod});
+
+		this.addTalibIndicator('ma7', 'ma', {optInTimePeriod : 7 , optInMAType : 0});
+		this.addTalibIndicator('ma25', 'ma', {optInTimePeriod : 25 , optInMAType : 0});
+		this.addTalibIndicator('ma99', 'ma', {optInTimePeriod : 99 , optInMAType : 0});
+
+		this.BULL_MOD_high = this.settings.BULL.mod_high;
+		this.BULL_MOD_low = this.settings.BULL.mod_low;
+		this.BEAR_MOD_high = this.settings.BEAR.mod_high;
+		this.BEAR_MOD_low = this.settings.BEAR.mod_low;
+
+		if(config.valProfit === undefined) config.valProfit = 1.75;
+		if(config.stoplost === undefined) config.stoplost = 0.00;
+
+		config.downwillbuy =  0.0275;
+		config.auto_buy = false;
+		rconfig = this.readConfig();
+		if(rconfig.buyPrice > 0) this.order.buy_price = rconfig.buyPrice;
+		if(rconfig.sellPrice > 0) this.order.sell_price = rconfig.sellPrice;
+
+	},
+	update : function(){
+		this.maSlow = this.talibIndicators.maSlow.result.outReal;
+		this.maFast = this.talibIndicators.maFast.result.outReal;
+		this.bbands = this.talibIndicators.bbands.result;
+		
+		this.BULL_RSI = this.talibIndicators.BULL_RSI.result.outReal;
+		this.BEAR_RSI = this.talibIndicators.BEAR_RSI.result.outReal;
+
+		this.adx = this.talibIndicators.adx.result.outReal;
+		this.ma7 = this.talibIndicators.ma7.result.outReal;
+		this.ma25 = this.talibIndicators.ma25.result.outReal;
+		this.ma99 = this.talibIndicators.ma99.result.outReal;
+
+
+		this.order.date = this.candle.start.unix();
+		
+	},
+	check : function(){
+		var price = this.candle.close;
+		var zone = 'none';
+		var priceUpperBB = this.bbands.outRealLowerBand + (this.bbands.outRealUpperBand - this.bbands.outRealLowerBand) / 100 * this.settings.BBtrend.upperThreshold;
+		var priceLowerBB = this.bbands.outRealLowerBand + (this.bbands.outRealUpperBand - this.bbands.outRealLowerBand) / 100 * this.settings.BBtrend.lowerThreshold;
+
+		if (price >= priceUpperBB) zone = 'high';
+		if ((price < priceUpperBB) && (price > priceLowerBB)) zone = 'middle';
+		if (price <= priceLowerBB) zone = 'low';
+
+		var isTrend = "none";
+
+		if(this.ma99 < this.ma25 && this.ma99 < this.ma7){
+			isTrend = "up";
+		}else if(this.ma99 > this.ma25 && this.ma99 > this.ma7){
+			isTrend = "down";
+		}
+
+		if( isTrend == "down" )
+		{
+			rsi = this.BEAR_RSI;
+			rsi_hi = this.settings.BEAR.high,
+			rsi_low = this.settings.BEAR.low;
+			//console.log(rsi, rsi_hi, rsi_low, isTrend)
+			// ADX trend strength?
+			if( this.adx > this.settings.ADX.high ) rsi_hi = rsi_hi + this.BEAR_MOD_high;
+			else if( this.adx < this.settings.ADX.low ) rsi_low = rsi_low + this.BEAR_MOD_low;
+				
+			
+
+		}else{
+			rsi = this.BULL_RSI;
+			rsi_hi = this.settings.BULL.high,
+			rsi_low = this.settings.BULL.low;
+			
+			// ADX trend strength?
+			//console.log(rsi, rsi_hi, rsi_low, isTrend)
+			if( this.adx > this.settings.ADX.high ) rsi_hi = rsi_hi + this.BULL_MOD_high;		
+			else if( this.adx < this.settings.ADX.low ) rsi_low = rsi_low + this.BULL_MOD_low;
+
+		}
+
+		//console.log(rsi)
+
+		if(rsi > rsi_hi && zone === "high"){
+			//console.log("Sell : ",rsi, isTrend)
+			this.targetOrder("sell")
+		}
+
+		if(rsi < rsi_low && zone === "low"){
+			//console.log("Buy : ",rsi,isTrend)
+			this.targetOrder("buy")
+		}
+
+	},
+
+	readConfig : function(){
+		this.filecache = __dirname + "/../markets/" + config.watch.asset+config.watch.currency+".json";
+		if (fs.existsSync(this.filecache)) {
+			var readJson = fs.readFileSync(this.filecache,"utf8");
+			return  JSON.parse(readJson);
+		}
+		return {};
+	},
+	readMarkets : function(){
+		cloudService = __dirname + "/../markets/cloud.json";
+
+		if (fs.existsSync(cloudService) && config.market24h === true) {
+			var markets = JSON.parse(fs.readFileSync(cloudService));
+			data = _.first(_.filter(markets, {symbol : config.watch.asset+config.watch.currency}));
+
+			btcusdt = _.first(_.filter(markets, {symbol : "BTCUSDT"}));
+
+			
+
+			if(btcusdt.priceChangePercent < -5 || btcusdt.priceChangePercent > 5){
+				return "sellall";
+			}
+
+			if(data.priceChangePercent > 25){
+				return "stopbuy";
+			}
+			if(data.priceChangePercent > 35){
+				return "sellall";
+			}
+			if(data.priceChangePercent < -35){
+				return "unlockbuy";
+			}
+		}
+
+		return false;
+
+	},
+
+	targetOrder : function(type){
+		var price = this.candle.close;
+		var profix = this.order.buy_price + (this.order.buy_price * (config.valProfit/100));
+		var amount = (this.order.balance / price ) - this.order.amount * config.downwillbuy;
+		var stoplost = this.order.buy_price - (this.order.buy_price * config.stoplost)
+		var readMarkets = this.readMarkets();
+
+		//var amount_next = this.order.amount * 0.01;
+		//console.log(this.order.buy_price, profix);
+
+		/*
+		Target Markets Sell All
+		*/
+		if(readMarkets === "sellall"){
+
+			//var unixtime = 60 * (240 * config.tradingAdvisor.candleSize);
+			this.order.block_time = this.candle.start.unix() + 84000;
+			this.short();
+			return true;
+		}
+
+		if(readMarkets === "stopbuy" && type == "buy"){
+			return false;
+		}
+		if(readMarkets === "unlockbuy" && type == "buy"){
+			this.order.block_time = 0;
+			this.long();
+			return true;
+		}
+
+		/*
+
+		if(price < stoplost && this.order.buy_price > 0 && config.stoplost > 0){
+
+			var unixtime = 60 * (240 * config.tradingAdvisor.candleSize);
+			if(this.maFast > this.maSlow) unixtime = unixtime * 2;
+
+			this.order.block_stoplost = this.candle.start.unix() + 84000 * this.order.block_stoplost_number;
+			this.order.block_stoplost_number = this.order.block_stoplost_number + 1;
+			console.log("stoplost");
+			this.short();
+			return true;
+		}
+		*/
+
+		/*
+		unlock Time
+		*/
+		if(this.order.date > this.order.block_time && this.order.block_time > 0){
+			console.log("Unlock Time");
+			this.order.block_time = 0;
+			this.order.balance = 0;
+		}
+
+		/*
+		if(this.order.date > this.order.block_stoplost && this.order.block_stoplost > 0){
+			console.log("Unlock Time Stop Lost");
+			this.order.block_stoplost = 0;
+			this.order.block_stoplost_number = 0;
+		}
+		*/
+
+		if(type === "sell"){
+			
+			if(this.order.date > this.order.exit_time && this.order.exit_time > 0){
+				this.order.exit_time = 0;
+				this.short();
+				return true;
+			}
+			if(config.valProfit > 0 && this.order.buy_price  > 0){
+
+				if(price > profix && this.order.buy_price > 0){
+					this.short();
+					return true;
+				}
+
+			}else{
+
+				this.short();
+				return true;
+			}
+			
+
+		}
+
+
+		if(type === "buy"){
+			
+			
+			
+			if(this.order.balance == 0 || config.auto_buy === true){
+				this.long();
+				return true;
+			}
+
+			if(amount > this.order.amount && this.order.balance > 0){
+				//console.log(this.order.amount, amount);
+				this.long();
+				return true;
+			}
+		}
+	},
+	long : function(){
+		/*
+		Buy
+		*/
+		
+		if(this.trend.direction !== "up"){
+			this.trend.direction = 'up';
+			this.advice('long');
+		}
+	},
+	short : function(){
+		/*
+		Sell
+		*/
+
+		if(this.trend.direction !== "down"){
+			this.trend.direction = 'down';
+
+			
+			this.advice('short');
+		}
+	},
+	onTrade : function(trade){
+		if(this.order.start_amount == 0) this.order.start_amount = trade.amount;
+		if(this.order.start_price == 0) this.order.start_price = trade.price;
+
+
+		this.order.finish_amount = trade.amount;
+		this.order.finish_price = trade.price;
+
+		this.order.profit_asset = (trade.amount - this.order.start_amount);
+		this.order.profit_btc = this.order.profit_asset * trade.price;
+		this.order.profit_start = this.order.start_price * (this.order.finish_amount - this.order.start_amount)
+
+		
+		
+		var unixtime = 60 * (config.tradingAdvisor.historySize * config.tradingAdvisor.candleSize);
+
+		if(trade.action === "buy"){
+
+			this.order.block_time = 0;
+			this.order.buy_price = trade.price;
+			this.order.exit_time = trade.date.unix() + (unixtime * 15);
+			if(config.tradingAdvisor.candleSize < 5){
+				this.order.exit_time  = trade.date.unix() + (84000 * 7)
+			}
+		}
+
+		if(trade.action === "sell"){
+
+			this.order.buy_price = 0;
+			this.order.sell_price = trade.price;
+			this.order.balance = trade.balance;
+			this.order.amount = trade.amount;
+			this.order.exit_time = 0;
+			this.order.block_time = trade.date.unix() + unixtime;
+
+		}
+		//console.log(this.order)
+		
+	},
+	end : function(){
+		console.log(this.order);
+	}
+};
+
+
+module.exports = strat;
