d9723716f214837423ecd84a902694adbc244029
Roller Davic <rollerdavic@gmail.com>
2019-03-14 05:54:51 +0700

    Update

--- strategies/MagicLive.js
+++ strategies/MagicLive.js
@@ -80,10 +80,6 @@ var strat = {
 
 		this.order.date = this.candle.start.unix();
 		
-		
-		
-	},
-	check : function(){
 		var sellPrice = this.buyprice + (this.buyprice * this.profit);
 
 		var readMarkets = this.readMarkets();
@@ -99,6 +95,10 @@ var strat = {
 			}
 		}
 
+		
+	},
+	check : function(){
+		
 	},
 
 	readConfig : function(){
